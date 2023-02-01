d548f446eae8529324267188ca217e9fd20a19f9
Shravun Sunkum <shravun@Shravuns-MacBook-Pro.local>
2023-02-01 11:50:14 -0500

    New Package.json

---
+++ iSight-MobileApp-main-2/VisionTalesAdmin/node_modules/react-quiz-component/lib/Locale.js
@@ -0,0 +1,22 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var defaultLocale = {
+  landingHeaderText: '<questionLength> Questions',
+  question: 'Question',
+  startQuizBtn: 'Start Quiz',
+  resultFilterAll: 'All',
+  resultFilterCorrect: 'Correct',
+  resultFilterIncorrect: 'Incorrect',
+  nextQuestionBtn: 'Next',
+  resultPageHeaderText: 'You have completed the quiz. You got <correctIndexLength> out of <questionLength> questions.',
+  resultPagePoint: 'You scored <correctPoints> out of <totalPoints>.',
+  singleSelectionTagText: 'Single Selection',
+  multipleSelectionTagText: 'Multiple Selection',
+  pickNumberOfSelection: 'Pick <numberOfSelection>'
+};
+var _default = defaultLocale;
+exports.default = _default;
